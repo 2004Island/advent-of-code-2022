6aaa5b2cbcf0ef5df0778740da00b1a97e2d9d47
Varun Sreedharan <varun@firecrest.org>
2022-12-03 19:37:58 -0800

    Add files via upload

--- day2/day2.py
+++ day2/day2.py
@@ -4,8 +4,11 @@
 with open('day2/input.txt') as f:
     lines = f.readlines()
 
+# Creating dictionaries for all the possible outcomes of RPS and the scores
 outcomes = {'A X': 4, 'A Y': 8, 'A Z': 3, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 7, 'C Y': 2, 'C Z': 6}
 outcomesp2 = {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
+
+# Iterates through input.txt and adds the scores to a list. The list is then summed and printed.
 score = sum([outcomes[x.strip('\n')] for x in lines])
 score2 = sum([outcomesp2[x.strip('\n')] for x in lines])
 print(score)
