3bcf276799c345ed18374277d08e4abf16b5c6f6
Varun Sreedharan <varun@firecrest.org>
2022-12-06 18:46:06 -0800

    Add files via upload

--- advent of code/day5/day5.py
+++ advent of code/day5/day5.py
@@ -7,9 +7,26 @@ with open('day5/input.txt') as f:
 crates = [['B', 'S', 'V', 'Z', 'G', 'P', 'W'], ['J', 'V', 'B', 'C', 'Z', 'F'], ['V', 'L', 'M', 'H', 'N', 'Z', 'D', 'C'], ['L', 'D', 'M', 'Z', 'P', 'F', 'J', 'B'],
 ['V', 'F', 'C', 'G', 'J', 'B', 'Q', 'H'],['G', 'F', 'Q', 'T', 'S', 'L', 'B'],['L', 'G', 'C', 'Z', 'V'],['N', 'L', 'G'],['J', 'F', 'H', 'C']]
 
-inst = lines[10]
-inst = inst.strip('move\n')
-inst = inst.replace(' from ', '-')
-inst = inst.replace(' to ', '-')
-inst = inst.split('-')
-print(inst)
+crates2 = [['B', 'S', 'V', 'Z', 'G', 'P', 'W'], ['J', 'V', 'B', 'C', 'Z', 'F'], ['V', 'L', 'M', 'H', 'N', 'Z', 'D', 'C'], ['L', 'D', 'M', 'Z', 'P', 'F', 'J', 'B'],
+['V', 'F', 'C', 'G', 'J', 'B', 'Q', 'H'],['G', 'F', 'Q', 'T', 'S', 'L', 'B'],['L', 'G', 'C', 'Z', 'V'],['N', 'L', 'G'],['J', 'F', 'H', 'C']]
+
+for i in lines[10:]:
+    inst = i
+    inst = inst.strip('move\n')
+    inst = inst.replace(' from ', '-')
+    inst = inst.replace(' to ', '-')
+    inst = inst.split('-')
+    inst = [int(x) for x in inst]
+
+    for x in range(inst[0]):
+        print(crates[inst[2]-1])
+        print(crates[inst[1]-1])
+        crates[inst[2]-1].append(crates[inst[1]-1][-1])
+        crates[inst[1]-1].pop()
+
+    [crates2[inst[2]-1].append(crates2[inst[1]-1][-1*i]) for i in range(inst[0],0,-1)]
+    [crates2[inst[1]-1].pop() for i in range(inst[0])]
+
+[print(x[-1]) for x in crates]
+print('\n')
+[print(x[-1]) for x in crates2]
---
+++ advent of code/day6/day6.py
@@ -0,0 +1,5 @@
+# Advent of Code [Dec 6 2022]
+# Varun Sreedharan 
+
+with open('day6/input.txt') as f:
+    lines = f.readlines()
